56837beed5d317f3b6214b79047b5ae96abd3499
Rei Hakurei <contact@bloom-juery.net>
2026-04-08 10:13:24 +0700

    modularize post and sort files by time, ascending

--- app.py
+++ app.py
@@ -2,7 +2,8 @@
 import json
 import time
 import yaml
-import datetime
+import pathlib
+from datetime import datetime
 import requests
 
 users = {}
@@ -24,12 +25,45 @@ def load_config():
   users = data['users']
   posts = data['posts']
 
+def process_post(post):
+  user = users.get(post['user'])
+  session = requests.Session()
+  if not user:
+    return
+  files = []
+  if post.get('files'):
+    files.extend(post['files'])
+  if post.get('folders'):
+    for folder_id in post['folders']:
+      folder_files = session.post('{}/api/drive/files'.format(user['host']), json={
+        'i': user['token'],
+        'folderId': folder_id,
+        'limit': 16,
+        'sinceId': '0',
+        'sort': None,
+      }).json()
+      folder_files.sort(key=lambda f: datetime.strptime(
+        f['createdAt'],
+        r'%Y-%m-%dT%H:%M:%S.%f%z',
+      ).timestamp())
+      files.extend(f['id'] for f in folder_files)
+
+  if len(files) > 16:
+    print('There are {} files found, exceeds software limit (16). Truncating the rest.'.format(len(files)))
+
+  session.post('{}/api/notes/create'.format(user['host']), json={
+    'i': user['token'],
+    'text': post['content'],
+    'fileIds': files[:16],
+  })
+
 def main():
   while True:
     ctime = time.time()
     rest_posts = [p for p in posts if ctime <= p['time']]
     post_times = list(set(p['time'] for p in rest_posts))
     if not post_times:
+      print('No more posts to process. Exiting.')
       break
     ntime = post_times[0]
     next_posts = [p for p in posts if abs(ntime - p['time']) <= 1e-6]
@@ -37,29 +71,7 @@ def main():
     ctime = time.time()
     time.sleep(ntime - ctime)
     for post in next_posts:
-      user = users.get(post['user'])
-      session = requests.Session()
-      if not user:
-        continue
-      files = []
-      if post.get('files'):
-        files.extend(post['files'])
-      if post.get('folders'):
-        for folder_id in post['folders']:
-          folder_files = session.post('{}/api/drive/files'.format(user['host']), json={
-            'i': user['token'],
-            'folderId': folder_id,
-            'limit': 16,
-            'sinceId': '0',
-            'sort': None,
-          }).json()
-          # folder_files.sort(key=lambda f: f)
-          files.extend(f['id'] for f in folder_files)
-      session.post('{}/api/notes/create'.format(user['host']), json={
-        'i': user['token'],
-        'text': post['content'],
-        'fileIds': files,
-      })
+      process_post(post)
 
 load_config()
 
